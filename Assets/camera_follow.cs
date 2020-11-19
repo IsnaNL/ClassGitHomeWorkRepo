8c80e82062da1955e5a9ae1fede5d61e1186bfdb
sahar <sahar.malter1@gmail.com>
2020-11-19 17:44:54 +0200

    added collectable cats, particle system and camera follow

---
+++ Assets/camera_follow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera_follow : MonoBehaviour
+{
+    public Vector3 offset;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void FixedUpdate()
+    { 
+    
+        Camera.main.transform.position = Vector3.Lerp(Camera.main.transform.position,  transform.position + offset,0.2f);
+    }
+    private void LateUpdate()
+    {
+    }
+}
