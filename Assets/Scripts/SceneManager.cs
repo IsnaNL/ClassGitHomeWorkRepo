1ec9749b5d65d48484903055065a8d27c910ec00
Amir Aharoni <Aharoniamirr@gmail.com>
2020-11-19 23:39:21 +0200

    Added Functioning UI screens and scene manager on top
    awaiting game completion for connections.

---
+++ Assets/Scripts/SceneManager.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject PauseMenu;
+    public GameObject HUD;
+    public GameObject CreditsMenu;
+
+    public static bool IsPlaying { get => UnityEditor.EditorApplication.isPlaying || Application.isPlaying; }
+    private void Awake()
+    {
+        if (PauseMenu == null || HUD == null || CreditsMenu == null)
+        {
+            Debug.LogError("some or all menus aren't connect to the SceneManager script in 'UI' Canvas");
+        }
+    }
+
+    void Start()
+    {
+        PauseScene();
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            PauseScene();
+        }
+    }
+    public void ResumeScene()
+    {
+        Time.timeScale = 1f;
+        if (PauseMenu != null)
+        {
+            PauseMenu.gameObject.SetActive(false);
+        }
+        if (CreditsMenu != null)
+        {
+            CreditsMenu.gameObject.SetActive(false);
+        }
+        HUD.gameObject.SetActive(true);
+
+    }
+    public void PauseScene()
+    {
+        if (HUD != null)
+        {
+            HUD.gameObject.SetActive(false);
+        }
+        if (CreditsMenu != null)
+        {
+            CreditsMenu.gameObject.SetActive(false);
+        }
+        PauseMenu.gameObject.SetActive(true);
+        if (IsPlaying)
+        {
+            Time.timeScale = 0f;
+        }
+    }
+    public void CreditsScene()
+    {
+        if (PauseMenu != null)
+        {
+            PauseMenu.gameObject.SetActive(false);
+        }
+        if (HUD != null)
+        {
+            HUD.gameObject.SetActive(false);
+        }
+        CreditsMenu.gameObject.SetActive(true);
+    }
+    public void QuitGame()
+    {
+
+        //splitting isPlaying to settle conflicts between editor and application
+
+        if (UnityEditor.EditorApplication.isPlaying)
+        {
+            UnityEditor.EditorApplication.isPlaying = false;
+        }
+        else
+        {
+            Application.Quit();
+        }
+    }
+}
