0b0779e75fe8d4439c79ba12f41c962e4ff98caa
sahar <sahar.malter1@gmail.com>
2020-11-19 17:51:49 +0200

    test

---
+++ Assets/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public static int score;
+    public Text score_text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        score_text.text = score.ToString();
+    }
+}
