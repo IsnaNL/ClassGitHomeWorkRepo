f539002c3d8b8ec766f5279a21ecfa2768539429
Nadav Litver <Nlitver@gmail.com>
2020-11-19 21:57:12 +0200

    Changes to Movement and Groundcol

--- Assets/Scripts/WitchMovement.cs
+++ Assets/Scripts/WitchMovement.cs
@@ -7,39 +7,42 @@ public class WitchMovement : MonoBehaviour
     private float HorInput;
     public float Fuel;
     public Vector2 Velocity;
-    public float gravity;
-    public float FuelUsage;
-    public float LiftForce;
-    public float FillRate;
-    public float HorMovementSpeed;
-    public LayerMask GroundLayerMask;
+    private float gravity;
+    public float fuelUsage;
+    public float liftForce;
+    public float MaxVelocity;
+    public float fillRate;
+    public float horMovementSpeed;
+    public LayerMask groundLayerMask;
     public float groundCheckDistance;
 
 
     void Update()
     {
+        Velocity = Vector2.ClampMagnitude(Velocity, MaxVelocity);
         if (!Isgrounded())
         {
             Velocity.y -= gravity * Time.deltaTime;
+           
         }
         HorInput = Input.GetAxis("Horizontal");
         if (HorInput != 0)
         {
-            Velocity.x += HorInput * HorMovementSpeed * Time.deltaTime;
+            Velocity.x += HorInput * horMovementSpeed * Time.deltaTime;
         }
         if (Input.GetButton("Jump"))
         {
             if(Fuel > 0)
             {
-                Fuel -= FuelUsage * Time.deltaTime;
-                Velocity.y += LiftForce * Time.deltaTime;
+                Fuel -= fuelUsage * Time.deltaTime;
+                Velocity.y += liftForce * Time.deltaTime;
             }
         }
         else
         {
             if(Fuel < 100)
             {
-                Fuel += FillRate * Time.deltaTime;
+                Fuel += fillRate * Time.deltaTime;
             }
         }
     }
@@ -49,15 +52,17 @@ public class WitchMovement : MonoBehaviour
     }
     public bool Isgrounded()
     {
-        RaycastHit2D hitGround = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, GroundLayerMask);
+        RaycastHit2D hitGround = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, groundLayerMask);
         Debug.DrawRay(transform.position, Vector2.down * groundCheckDistance, Color.red);
         if (hitGround)
         {
-            Velocity *= 0.5f;
+            Velocity.y *= 0.99f;
+            gravity = 0;
             return true;
         }
         else
         {
+            gravity = 5;
             return false;
         }
     }
