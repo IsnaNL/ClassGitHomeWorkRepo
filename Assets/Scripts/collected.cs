75f7cf332fdd86428eb320305a5e85509cca6fa1
sahar <sahar.malter1@gmail.com>
2020-11-28 11:01:51 +0200

    Added SFX + Potions

--- Assets/Scripts/collected.cs
+++ Assets/Scripts/collected.cs
@@ -6,10 +6,13 @@ public class collected : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        GetComponent<Collider2D>().enabled = false;
         GetComponent<Animator>().enabled=true;
         GameManager.score++;
+        GetComponent<AudioSource>().pitch=Random.Range(0.5f,2f);
+        GetComponent<AudioSource>().Play();
         Destroy(gameObject, 0.5f);
-      
+       
     }
    
 }
---
+++ Assets/potions.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class potions : MonoBehaviour
+{
+    public float Fuel_To_Add; 
+    //public float Speed_To_Add; 
+    public float Speed_time; 
+    public float Food_Radios; 
+
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        collision.GetComponent<AudioSource>().Play();
+        collision.GetComponent<Collider2D>().enabled= false;
+        collision.GetComponent<SpriteRenderer>().enabled= false;
+        Destroy(collision.gameObject,0.5f);
+        if (collision.CompareTag("Fuel"))
+        {
+            GetComponent<WitchMovement>().Fuel += Fuel_To_Add;
+        }
+        if (collision.CompareTag("Food"))
+        {
+            Food();
+
+        }
+        if (collision.CompareTag("Speed"))
+        {
+            StartCoroutine(Speed_change());
+
+        }
+
+    }
+
+    public void Food()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, Food_Radios);
+
+        foreach (Collider2D hit in colliders)
+        {
+            if (hit.CompareTag("cat"))
+            {
+              LeanTween.move(hit.gameObject, transform.position, 0.5f);
+            }
+        }
+
+    }
+
+    public IEnumerator Speed_change()
+    {
+
+        GetComponent<WitchMovement>().horMovementSpeed *= 5f;
+        GetComponent<WitchMovement>().liftForce *= 5f;
+        yield return new WaitForSeconds(Speed_time);
+        GetComponent<WitchMovement>().horMovementSpeed /= 5f;
+        GetComponent<WitchMovement>().liftForce /= 5f;
+    }
+
+}
+
